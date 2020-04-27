95c8bafbc6969ccbf58f6a659e86d5facc416520
Morphix <pmjakubiak13@gmail.com>
2020-04-27 21:18:02 +0200

    Numerator program fixed (u can't use string in this exercise :=( ).

--- 4_Pliki/numerator.cpp
+++ 4_Pliki/numerator.cpp
@@ -3,15 +3,16 @@
 int main(int argc, char *argv[]) {
     std::ifstream input(argv[1]);
     std::ofstream  output(argv[2]);
-    std::string line;
     int counter = 1;
 
-    while (std::getline(input, line)) {
-        output << counter++ << " " << line << "\n";
+    for(char p = '\n', c; input.get(c); p = c){
+        if(p == '\n')
+            output << counter++ << " " << c;
+        else
+            output << c;
     }
 
     output.close();
     input.close();
-
     return 0;
 }
